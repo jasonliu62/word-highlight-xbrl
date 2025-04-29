6b7341454246c1cb8a495ac7f42aa39110c2491d
Byung Hun Lee <michael@focusuniversal.com>
2025-04-29 13:42:29 -0700

    initial commit

---
+++ highlight/ThisAddIn.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Word = Microsoft.Office.Interop.Word;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Word;
+
+namespace highlight
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            this.Application.WindowSelectionChange += new Word.ApplicationEvents4_WindowSelectionChangeEventHandler(Application_WindowSelectionChange);
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        private void Application_WindowSelectionChange(Word.Selection Sel)
+        {
+            string selectedText = Sel.Text.Trim();
+
+            if (!string.IsNullOrEmpty(selectedText))
+            {
+                System.Windows.Forms.MessageBox.Show("Selected Text (Fact): " + selectedText, "Fact Annotation");
+                // Later: show a custom input form instead of MessageBox
+            }
+        }
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
